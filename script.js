eb83565419430e873b45c23f66bf4695e17db3a8
lukmanuthmanabubakr <123726979+lukmanuthmanabubakr@users.noreply.github.com>
2023-06-14 07:45:04 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+// alert('Hello world!');
+let js = 'amazing'
+// if(js === 'amazing') alert('Javascript is fun')
+console.log(40 + 8 + 23);
+console.log('Abubakr');
+
+let firstName = 'Matilda';
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let myFirstJob = 'Teacher';
+let myCurrentJob = 'Programmer';
+
+console.log(myFirstJob);
+
+let myFirstCar = 'Benz';
+let myCurrentCar = 'Lexux';
+
+console.log(myFirstCar, myCurrentCar);
